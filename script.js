3d5a75a6dd3141787f0e131c277474436d853dcb
William <willspri@gmail.com>
2024-07-10 08:39:55 +1200

    win messages

--- script.js
+++ script.js
@@ -60,10 +60,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const announce = (type) => {
         switch (type) {
             case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                announcer.innerHTML = '<span class="playerO">O</span> Wins';
                 break;
             case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                announcer.innerHTML = '<span class="playerX">X</span> Wins';
                 break;
             case TIE:
                 announcer.innerText = 'Tie';
