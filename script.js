a77be66d2bcac7407ac5e98c608fa0a797affe34
William <willspri@gmail.com>
2024-07-10 09:15:14 +1200

    comments

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     /*
-        Indexes within the board
+        Board indexes
         [0] [1] [2]
         [3] [4] [5]
         [6] [7] [8]
